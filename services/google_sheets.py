c14c81c230fa63c758969a143cff6bffbb6fa889
Savikj, Mladen <mladen.savikj@gmail.com>
2026-05-01 22:37:09 +0200

    created variable file and configured google_sheets.py better

--- services/google_sheets.py
+++ services/google_sheets.py
@@ -1,20 +1,19 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from config import SECRETS_PATH, SCOPES, SHEET_KEY, TAB_NAME
 
-def get_client(secrets_path="secrets.json"):
-    scope = [
-        "https://www.googleapis.com/auth/spreadsheets",
-        "https://www.googleapis.com/auth/drive"
-    ]
+# Google Sheets API client setup
+def get_client(secrets_path=SECRETS_PATH):
+    scope = SCOPES
 
     creds = Credentials.from_service_account_file(
-        "secrets/secrets.json",
+        secrets_path,
         scopes=scope
     )
     client = gspread.authorize(creds)
     return client
 
-
+# Get data from a specific sheet
 def get_sheet():
     client = get_client()
-    return client.open("exercise-data").open("exercise-data")
+    return client.open_by_key(SHEET_KEY).worksheet(TAB_NAME)
