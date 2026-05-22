67e04e6b6bd86025b8d140d3a8b2fdcb916f8e87
Savikj, Mladen <mladen.savikj@gmail.com>
2026-05-22 23:40:35 +0200

    setup to ensure dummy_data paths are working after deploying

--- services/data_layer/read_dummy_data.py
+++ services/data_layer/read_dummy_data.py
@@ -1,12 +1,14 @@
 import pandas as pd
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent  # adjust if needed
+
 
 def get_dummy_master_df():
-    return pd.read_csv(
-        "dummy_data/exercise_master_data.csv"
-    )
+    path = BASE_DIR / "dummy_data" / "exercise_master_data.csv"
+    return pd.read_csv(path)
 
 
 def get_dummy_exercise_df():
-    return pd.read_csv(
-        "dummy_data/exercise_data.csv"
-    )
+    path = BASE_DIR / "dummy_data" / "exercise_data.csv"
+    return pd.read_csv(path)
