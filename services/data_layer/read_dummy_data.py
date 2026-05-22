e36150d8079343a8b3012ea23204354aa1dc81c0
Savikj, Mladen <mladen.savikj@gmail.com>
2026-05-22 23:46:39 +0200

    another fix for the dummy_data path

--- services/data_layer/read_dummy_data.py
+++ services/data_layer/read_dummy_data.py
@@ -1,8 +1,7 @@
-import pandas as pd
 from pathlib import Path
+import pandas as pd
 
-BASE_DIR = Path(__file__).resolve().parent.parent  # adjust if needed
-
+BASE_DIR = Path(__file__).resolve().parents[2]
 
 def get_dummy_master_df():
     path = BASE_DIR / "dummy_data" / "exercise_master_data.csv"
